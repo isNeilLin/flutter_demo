2ac6ed37ba8081b4bea2a3fd1d641c0f8367f6e2
Lin Pengfei <1264766856@qq.com>
2018-03-14 11:06:05 +0800

    FriendlyChat

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,165 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-import 'navigation_icon_view.dart';
+import 'common.dart';
 
-void main()=>runApp(new MyApp());
+// 程序入口
+void main() => runApp(new FriendlyChatApp());
+const _name = 'Neil Lin';
 
-class MyApp extends StatelessWidget {
+// 根级微件
+class FriendlyChatApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    
+  Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Welcome to Flutter',
-      theme: new ThemeData(
-        primaryColor: Colors.red,
-      ),
-      home: new RandomWords(),
+      title: 'FriendlyChat',
+      home: new ChatScreen(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class ChatScreen extends StatefulWidget {
   @override
-  createState() => new RandomWordsState();
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new ChatStatefulScreen();
+  }
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = new Set<WordPair>();
-  final _bigFont = const TextStyle(fontSize: 18.0);
-  var title = 'Saved Suggestions';
-  void _pushSaved(){
-    Navigator.of(context).push(
-      new MaterialPageRoute(
-        builder: (context) {
-          final tiles = _saved.map((pair){
-            return new ListTile(
-              title: new Text(
-                pair.asPascalCase,
-                style: _bigFont,
-              ),
-            );
-          });
-          final divided = ListTile
-            .divideTiles(
-              context: context,
-              tiles: tiles,
-            )
-            .toList();
-          return new Scaffold(
-            appBar: new AppBar(
-              title: new Text(title),
-              actions: <Widget>[
-                new PopupMenuButton<String>(
-                  onSelected: (String value){
-                    setState(()=>title=value);
-                  },
-                  itemBuilder: (BuildContext context)=><PopupMenuItem<String>>[
-                    new PopupMenuItem<String>(
-                      value: '选项1的value',
-                      child: new Text('选项一')
-                    ),
-                    new PopupMenuItem<String>(
-                      value: '选项2的value',
-                      child: new Text('选项二')
-                    ),
-                  ],
-                )
-              ],
+class ChatStatefulScreen extends State<ChatScreen> with TickerProviderStateMixin {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = new TextEditingController();
+  bool _isComposing = false;
+
+  void _handlerSubmited(String text) {
+    _textController.clear();
+    
+    ChatMessage message = new ChatMessage(
+      text: text,
+      animationController: new AnimationController(
+        duration: new Duration(milliseconds: 300),
+        vsync: this
+      ),
+    );
+    setState((){
+      _messages.insert(0, message);
+      _isComposing = false;
+    });
+    message.animationController.forward();
+  }
+
+  void _change(String text) {
+    setState((){
+      _isComposing = text.length > 0;
+    });
+  }
+
+  @override
+  void dispose() {
+    for (ChatMessage message in _messages) {
+      message.animationController.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('FriendlyChat'),
+      ),
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+            child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemCount: _messages.length,
+              itemBuilder: (_,index) => _messages[index],
             ),
-            body: new ListView(children: divided,),
-          );
-        }
+          ),
+          new Divider(height: 1.0),
+          new Container(
+            decoration: new BoxDecoration(
+              color: Theme.of(context).cardColor
+            ),
+            padding: new EdgeInsets.only(left: 8.0),
+            child: _buildTextComposer(),
+          )
+        ],
       )
     );
   }
 
-  Widget _buildSuggestions(){
-    return new ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return new Divider();
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Row(
+        children: <Widget>[
+          new Flexible(
+            child: new TextField(
+              controller: _textController,
+              style: new TextStyle(
+                fontSize: 18.0,
+                color: Colors.black,
+              ),
+              onSubmitted: _handlerSubmited,
+              onChanged: (String text) {
+                _change(text);
+              },
+              decoration: new InputDecoration.collapsed(
+                hintText: 'Send a message',
+                border: const UnderlineInputBorder(),
+              ),
+            ),
+          ),
+          new Container(
+            margin: new EdgeInsets.symmetric(horizontal: 4.0),
+            child: new IconButton(
+              icon: new Icon(Icons.send),
+              onPressed: _isComposing ?
+                () => _handlerSubmited(_textController.text) :
+                null,
+            ),
+          )
+        ],
+      )
     );
   }
+}
 
-  Widget _buildRow(WordPair pair){
-    final alreadySaved = _saved.contains(pair);
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _bigFont,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: (){
-        setState((){
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.animationController});
+  final String text;
+  final AnimationController animationController;
   @override
   Widget build(BuildContext context){
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Startup Name generate'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
-        ],
+    return new SizeTransition(
+      sizeFactor: new CurvedAnimation(
+        parent: animationController,
+        curve: Curves.easeOut
       ),
-      body: _buildSuggestions(),
+      axisAlignment: 0.0,
+      child: new Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: new CircleAvatar(child: new Text(_name[0]),),
+            ),
+            new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text(_name,style: Theme.of(context).textTheme.subhead),
+                new Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: new Text(text,style: Theme.of(context).textTheme.body1),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
